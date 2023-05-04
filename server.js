6030d9e9850ef7d66b839a4966945ceb744bdc26
Parker <parkerlamarbrook@gmail.com>
2023-05-03 18:49:02 -0600

    added get requests

--- server.js
+++ server.js
@@ -4,14 +4,35 @@ const fs = require('fs');
 const path = require('path');
 const notesDB = require('./db/db.json');
 
-// initialize express
+// import express
 const express = require('express');
+// create express instance
 const app = express();
 
-// use express
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+
+// get-request handling:
+// respond with index.html when a user visits the root/home page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// wildcard to handle all non-specified endpoints with index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// respond with notes.html when a user visits the notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+
+
 
 
 
